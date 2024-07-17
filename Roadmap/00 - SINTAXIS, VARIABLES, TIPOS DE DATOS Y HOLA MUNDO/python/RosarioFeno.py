d58dd1ab51838b70a4a00e8f7c8b1eefb212c74f
RosarioFeno <rosariofenoglio98@gmail.com>
2024-07-17 20:35:48 -0300

    Update RosarioFeno.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/RosarioFeno.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/RosarioFeno.py
@@ -3,3 +3,11 @@
 """ esto es un comentario en una linea"""
 
 my_variable= "Mi variable" 
+my_variable= "nuevo valor de mi variable" 
+
+MY_CONSTANT = "Mi constante" 
+MY_CONSTANT = "ÑMEKFWFW"
+
+my_int = 1
+my_float = 1.5
+my_bool = 
