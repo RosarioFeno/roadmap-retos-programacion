8c2bfbac09db3c75e2baefa7af39b82b756471e5
RosarioFeno <rosariofenoglio98@gmail.com>
2024-07-18 14:19:53 -0300

    ROFENO.PY

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Rofeno.py
@@ -0,0 +1,51 @@
+ https://python.org/
+#comentario en una linea
+#estos se utilizan para hacer comentarios de una linea
+
+"""para hacer comentarios en varias lineas se utilizan 
+las comillas dobles"""
+
+'''o las comillas
+simples'''
+
+#VARIABLES# 
+"""son nombres simbólicos que se utilizan para almacenar
+ y hacer referencia a valores en el programa. Las variables
+  pueden contener diferentes tipos de datos y sus valores pueden cambiar
+   a lo largo de la ejecución del programa"""
+
+#primero una variable: (por cada nueva palabra todo se escribe en minuscula y con el _)SNAKE
+my_variable= "mi variable" #que es? es un dato que cambia, que va mutando. le damos el valor que querramos
+my variable= "HOLA"
+
+#TIPOS DE VARIABLES
+
+#ENTERA (INT)     SIRVE PARA REPRESENTAR NUMEROS ENTEROS
+mi_entero = 25
+
+#FLOTANTE  (FLOAT) SIRVE PARA REPRESENTAR NUMEROS DECIMALES
+
+mi_flotante = 3,14
+My_float = 3,14
+
+#BOOLEANO (BOOL)   SIRVE PARA REPRESENTAR VERDADERO O FALSO
+
+my_bool = false
+my_bool = true
+
+#CADENA DE TEXTO (STR)
+
+my_string = "mi cadena de texto"
+my_other_string = "otra cadena de texto"
+
+print = (Hola,python)
+
+
+#CONSTANTES
+
+MY CONSTANT = " MI CONSTANTE"    """se usa una /convención/ para indicar que algo
+ es una constante. Esto significa que se acuerda entre
+  los programadores escribir el nombre en mayúsculas"""
+  MY CONSTANT = 3,14
+
+
